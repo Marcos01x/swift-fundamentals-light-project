a7699012c026db6e6241aecd74afb2b697d8751e
Marcos Arroyo <marcosarroyog@gmail.com>
2023-10-22 15:22:04 -0600

    add button title logic

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -9,19 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lightButton: UIButton!
     var lightOn = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
 
     fileprivate func updateUI() {
         if lightOn {
             view.backgroundColor = .white
+            lightButton.setTitle("Off", for: .normal)
         } else {
             view.backgroundColor = .black
+            lightButton.setTitle("On", for: .normal)
         }
     }
     
